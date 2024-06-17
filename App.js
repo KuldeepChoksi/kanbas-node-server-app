6642c1ffc75ae0a1008c904e65d96fbeb827a419
Kuldeep Choksi <KuldeepChoksi@Dragon-Mac.local>
2024-06-17 13:22:12 -0400

    a6 server

--- App.js
+++ App.js
@@ -5,8 +5,15 @@ import cors from "cors";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
+import mongoose from "mongoose";
+import "dotenv/config";
+import UserRoutes from "./Users/routes.js";
 
 
+ 
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -16,4 +23,6 @@ app.listen(process.env.PORT || 4000);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
+UserRoutes(app);
+
 
